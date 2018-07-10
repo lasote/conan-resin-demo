b8dcc4111e24c1388f6c1eea4e2e745ca9aa1b21
Luis Martinez de Bartolome <lasote@gmail.com>
2018-07-10 17:18:44 +0200

    Regenerate pass

--- mosquitto/run.sh
+++ mosquitto/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 mkdir -p /data/mosquitto
 
+
+
+
 if [ ! -f /data/mosquitto/mosquitto.conf ]; then
     echo "Copying configuration to /data/mosquitto"
     cp bin/mosquitto.conf /data/mosquitto/mosquitto.conf
 fi
 
 if [ ! -f /data/mosquitto/mosquitto_pass ]; then
+    touch bin/mosquitto_pass
+    bin/mosquitto_passwd -b bin/mosquitto_pass $MOSQUITTO_USERNAME $MOSQUITTO_PASS
     echo "Copying mosquitto_pass to /data/mosquitto"
     cp bin/mosquitto_pass /data/mosquitto/mosquitto_pass
 fi
